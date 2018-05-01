4e20739e0e579dbf121d92924ebe806f830d53ca
Dan Cannova <dan.cannova@apexlearning.com>
2018-05-01 13:57:02 -0700

    Carousel is workinggsgs

--- my-profile-resume/src/public/components/resume/projects/carousel.component.ts
+++ my-profile-resume/src/public/components/resume/projects/carousel.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'resume-carousel',
   templateUrl: './carousel.component.html',
-  // styleUrls: ['./carousel.component.css'],
+  styleUrls: ['./carousel.component.css'],
   preserveWhitespaces: false
 })
 export class CarouselComponent {
 	@Input() images: Array<string>;
-  slide: number = 0;
+	@Input() id: string;
+	@Input() backgroundColor: string = 'transparent';
 
   constructor() { }
 
